78318be24149bd1097b843b20d588412a868fc5c
tormichael <tormichael@gmail.com>
2015-09-16 15:22:56 +0300

    add console output

--- ProgramJournal.cs
+++ ProgramJournal.cs
@@ -11,7 +11,9 @@ namespace CommandAS.Tools
   /// error, message, trace, debug, warning and simple info.
   /// 
   /// M.Tor
-  /// 15.07.2015
+  /// 
+  ///     created - 15.07.2015
+  /// last update - 16.09.2015
   /// </summary>
   public class ProgramJournal
   {
@@ -26,7 +28,9 @@ namespace CommandAS.Tools
     }
 
     private TextBox _txb;
+    private string _fileName;
     private StreamWriter _sw;
+    private bool _isConsleOutput;
 
     public TextBox OutputTextBox
     {
@@ -37,41 +41,70 @@ namespace CommandAS.Tools
     {
       set
       {
-        if (_sw != null)
-          _sw.Close();
-        if (value != null && value.Length > 0)
-          _sw = new StreamWriter(value, true);
+        _fileName = value;
       }
     }
 
+    public bool IsConsoleOutput
+    {
+      get { return _isConsleOutput;  }
+      set { _isConsleOutput = value; }
+    }
     /// <summary>
     /// Output debug information to format:
-    /// [<current date> <current time>] <text>
+    /// [_current_date _current_time] _text
+    /// Not write to the log file, to display only.
     /// </summary>
     public string Debug
     {
       set { Write(null, value, Type.Debug); }
     }
+
+    /// <summary>
+    /// Output trace information to format:
+    /// {_current_time>} _text
+    /// Not write to the log file, to display only.
+    /// </summary>
     public string Trace
     {
       set { Write(null, value, Type.Trace); }
     }
 
+    /// <summary>
+    /// Output trace information to format:
+    /// _text
+    /// Write to the log file and show to display.
+    /// </summary>
     public string Info
     {
       set { Write(null, value, Type.Info); }
     }
 
+    /// <summary>
+    /// Output message information to format:
+    /// [_current_date _current_time] _text
+    /// Write to the log file and show to display.
+    /// </summary>
     public string Message
     {
       set { Write(string.Empty, value, Type.Message); }
     }
 
+    /// <summary>
+    /// Output warning information to format:
+    /// WARNING {_current_time>} _text
+    /// Write to the log file and show to display.
+    /// </summary>
     public string Warning
     {
       set { Write("warning", value, Type.Warning); }
     }
 
+    /// <summary>
+    /// Output error information to format:
+    /// ERROR [_current_date _current_time] _text
+    /// Write to the log file and show to display.
+    /// </summary>
     public string Error
     {
       set { Write("error", value, Type.Error); }
@@ -81,9 +114,13 @@ namespace CommandAS.Tools
     {
       _txb = null;
       _sw = null;
+      _isConsleOutput = false;
     }
 
-    public void Init()
+    /// <summary>
+    /// Clear TextBox contents, if it's defined.
+    /// </summary>
+    public void ClearDisplay()
     {
       if (_txb != null)
       {
@@ -92,6 +129,63 @@ namespace CommandAS.Tools
       }
     }
 
+    public void RefreshDisplay()
+    {
+      if (_txb != null)
+      {
+        _txb.Select(_txb.Text.Length, 0);
+        _txb.Refresh();
+      }
+    }
+
+    /// <summary>
+    /// Begin write block to log file.
+    /// If need opened file.
+    /// </summary>
+    /// <param name="aMessage">message text</param>
+    public void Begin(string aMessage)
+    {
+      Begin();
+      Message = aMessage;
+    }
+    /// <summary>
+    /// Begin write block to log file.
+    /// If need opened file.
+    /// </summary>
+    public void Begin()
+    {
+      if (_fileName != null && _fileName.Length > 0 && _sw == null)
+        _sw = new StreamWriter(_fileName, true);
+    }
+
+    /// <summary>
+    /// Commit write block - close log file.
+    /// </summary>
+    /// <param name="aMessage">message text</param>
+    public void Commit(string aMessage)
+    {
+      Message = aMessage;
+      Commit();
+    }
+    /// <summary>
+    /// Commit write block - close log file.
+    /// </summary>
+    public void Commit()
+    {
+      if (_sw != null)
+      {
+        _sw.Close();
+        _sw = null;
+      }
+    }
+
+    /// <summary>
+    /// Show output information to the display and write to the log file,
+    /// if they defined.
+    /// </summary>
+    /// <param name="aTitle"></param>
+    /// <param name="aText"></param>
+    /// <param name="aType"></param>
     public void Write(string aTitle, string aText, ProgramJournal.Type aType)
     {
 
@@ -104,13 +198,13 @@ namespace CommandAS.Tools
         {
           case Type.Debug:
           case Type.Error:
+          case Type.Message:
             dtStr = "[" + dt.ToShortDateString()
                 + " " + dt.ToShortTimeString() + "]";
             break;
           case Type.Trace:
           case Type.Warning:
-          case Type.Message:
-            dtStr = "{" + dt.ToShortTimeString() + "}";
+            dtStr = "{" + dt.ToLongTimeString() + "}";
             break;
         }
 
@@ -133,8 +227,14 @@ namespace CommandAS.Tools
       }
 
       if (_sw != null && aType != Type.Debug && aType != Type.Trace)
+      {
         _sw.WriteLine(aText);
+      }
 
+      if (_isConsleOutput && aType != Type.Debug && aType != Type.Trace)
+      {
+        System.Console.WriteLine(aText);
+      }
 
     }
   }
